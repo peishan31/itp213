69cca455e7158e539fe06b4645e2d9d26484ff3d
Unknown <171846z@mymail.nyp.edu.sg>
2018-12-26 14:43:49 +0800

    testing

--- ITP213/DAL/FeedbackDAO.cs
+++ ITP213/DAL/FeedbackDAO.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
+using System.Text;
 using System.Web;
 
 namespace ITP213.DAL
--- ITP213/DAL/ResponseDAO.cs
+++ ITP213/DAL/ResponseDAO.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
+using System.Text;
 using System.Web;
-
 namespace ITP213.DAL
 {
     public class ResponseDAO
